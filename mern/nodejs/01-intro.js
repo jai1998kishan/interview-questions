71504e02342de7eda92d267e1fdb64382d9861ca
Jaikishan <jaikishangupta754@gmail.com>
2026-09-22 11:44:06 +0530

    nodejs start

---
+++ mern/nodejs/01-intro.js
@@ -0,0 +1,26 @@
+/*
+
+1. What is Node.js?
+Answer:
+
+Node.js is an open-source, cross-platform JavaScript runtime environment that allows developers to execute JavaScript code outside the browser.
+
+It is built on Google's V8 JavaScript engine and is mainly used for building fast, scalable, and real-time applications like APIs, chat applications, streaming services, and backend systems.
+
+Key features:
+
+Asynchronous and non-blocking I/O
+
+Event-driven architecture
+
+Single-threaded event loop
+
+Large package ecosystem (npm)
+
+High performance for I/O-heavy applications
+
+
+
+
+
+*/
